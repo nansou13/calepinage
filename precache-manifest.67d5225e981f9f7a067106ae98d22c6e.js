bd6fa166c933d03ad1cb3eb3351c74729396eee5
Nans <nans.gigan@gmail.com>
2019-05-14 16:57:52 +0200

    Updates

---
+++ precache-manifest.67d5225e981f9f7a067106ae98d22c6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "803f2a296fce7f3b04ed",
+    "url": "/calepinage/static/js/runtime~main.803f2a29.js"
+  },
+  {
+    "revision": "5157e2dbc0621bdc87e5",
+    "url": "/calepinage/static/js/main.5157e2db.chunk.js"
+  },
+  {
+    "revision": "d9658763febdb1b1c52d",
+    "url": "/calepinage/static/js/2.d9658763.chunk.js"
+  },
+  {
+    "revision": "5157e2dbc0621bdc87e5",
+    "url": "/calepinage/static/css/main.0dac307d.chunk.css"
+  },
+  {
+    "revision": "f6309fddbb75c0fc92714c01a5a91a56",
+    "url": "/calepinage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65b0f34c8c0e9f2e8c7a55cc25c7323f.js"
+  "/calepinage/precache-manifest.67d5225e981f9f7a067106ae98d22c6e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/calepinage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
