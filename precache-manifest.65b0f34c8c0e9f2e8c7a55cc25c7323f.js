b1547e1879b8f3984c95b1adb43bd707a9802723
Nans <nans.gigan@gmail.com>
2019-05-14 16:54:04 +0200

    Updates

---
+++ precache-manifest.65b0f34c8c0e9f2e8c7a55cc25c7323f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "257a45f1dc3f0429980d",
+    "url": "/static/js/main.257a45f1.chunk.js"
+  },
+  {
+    "revision": "5e67eb5baa7aa341e4e9",
+    "url": "/static/js/2.5e67eb5b.chunk.js"
+  },
+  {
+    "revision": "257a45f1dc3f0429980d",
+    "url": "/static/css/main.0dac307d.chunk.css"
+  },
+  {
+    "revision": "e5442c89150ae36575fedb443ee30239",
+    "url": "/index.html"
+  }
+];
